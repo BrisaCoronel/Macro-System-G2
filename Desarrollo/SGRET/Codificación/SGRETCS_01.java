fcb6920315b55e53c5cf846a5cc4a30b874675d9
Renzo-Omar-Valega-Vidarte <renzo.valega@unmsm.edu.pe>
2023-05-05 20:41:39 -0500

    Archivos fantasmas
    Agregando ejemplos para los proyectos imaginarios

---
+++ Desarrollo/SGRET/Codificación/SGRETCS_01.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sgret.cs_01;
+
+/**
+ *
+ * @author BRISA
+ */
+public class SGRETCS_01 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Desarrollo/SGRET/Codificación/SGRETCS_02.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sgret.cs_02;
+
+/**
+ *
+ * @author BRISA
+ */
+public class SGRETCS_02 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

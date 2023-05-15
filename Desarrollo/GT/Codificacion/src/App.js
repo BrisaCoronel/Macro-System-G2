612b2ec30aa45bff8b2000d5ffb19f1e622f6ba2
FrancoAMR <franco.matos@unmsm.edu.pe>
2023-05-14 19:28:27 -0500

    Alineamiento de contenedores

--- Desarrollo/GT/Codificacion/src/App.js
+++ Desarrollo/GT/Codificacion/src/App.js
@@ -1,21 +1,27 @@
 import React, {useState} from "react";
 import Modal from "./Componentes/Modal";
-import {Header, Contenedorprincipal, Contenedorsec} from "./Componentes/Contenedor-principal";
+import {Contenedorprincipal, Contenedorsec, Header} from "./Componentes/Contenedor-principal";
 function App() {
   const [estadoModal, cambiarEstadoModal]  = useState(true);
   return (
     <div>
+      
       <Header 
       linkUrl="#"
       imagen="logo" 
       altText="Logo Ejemplo" />
-      <Contenedorprincipal 
-      num="800" />
+      <div>
+        <Contenedorprincipal 
+        num="800"
+        imagen= "gusano"/>
+      </div>
+      
       <Modal
         estado = {estadoModal}
         cambiarEstado={cambiarEstadoModal}
       />
       <Contenedorsec />
+      
     </div>
   );
 }
